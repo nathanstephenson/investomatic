8fa9d34d3116a380c454fd2e2b8da06858d82c6f
nathanstephenson <nathans261@gmail.com>
2022-04-09 05:48:56 +0100

    refactored

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,7 @@ import { openAiAPIKey, alpacaAPIKey, alpacaKeyID } from "./secrets"
 
 import { scrape as twitterScrape } from "./sources/twitter"
 import { Ticker } from "./classes"
+import { addTickersToMap, filterToAboveAverage } from "./utils"
 
 const configuration = new Configuration({
 	apiKey: openAiAPIKey
@@ -19,6 +20,10 @@ const alpaca = new Alpaca({
 	paper: true
 })
 
+const sourceMultipliers = {
+	"twitter": 1,
+}
+
 export const investomatic = functions.https.onRequest(async (request, response) => {
 
 	
@@ -27,37 +32,32 @@ export const investomatic = functions.https.onRequest(async (request, response) 
 	response.send("You probably shouldn't see this")
 })
 
-export const helloWorld = functions.https.onRequest(async (request, response) => {
-	const sourceMultipliers = {
-		"twitter": 1,
-	}
+export const scraping = functions.https.onRequest(async (request, response) => {
 
 	const tickers: Map<string, Ticker> = new Map<string, Ticker>()
 
 	// GATHER DATA
-	const twitter = await twitterScrape()
-	twitter.forEach((ticker) => {
-		const name = ticker.getName()
-		tickers.set(name, tickers.get(name) ?
-			tickers.get(name)!.multiplyRating(sourceMultipliers["twitter"] * ticker.getRating()) 
-			: new Ticker(name).multiplyRating(sourceMultipliers["twitter"] * ticker.getRating())
-		)
-	})
+	const twitterTickers = await twitterScrape()
+	addTickersToMap(twitterTickers, tickers, sourceMultipliers["twitter"])
 	console.log("added tweets to map")
 
 	// POST PROCESSING
-	let averageScore = 0
-	Object.values(tickers).forEach((ticker) => averageScore += ticker.getRating())
-	averageScore = averageScore/Object.values(tickers).length
+	const tickersList = filterToAboveAverage(tickers)
 
-	const filteredTickers = Object.keys(tickers).filter((ticker) => tickers.get(ticker)!.getRating()>averageScore)
-	console.log("filtered below average tickers out of map")
+	console.log("Out of the tickers " + tickers.values() + " these ones are above average in rating: " + tickersList)
 
-	const tickersList = filteredTickers.toString().replace(",", "\n")
+	const stocksToBuy = await getChoicesFromGPT(tickersList)
+
+	await makeOrder(stocksToBuy, tickers, response)
+});
+
+
+async function getChoicesFromGPT(tickersList: string[]) {
+	const filteredTickers = tickersList.toString().replace(",", "\n")
 	console.log("tickers selected: " + tickersList)
 
 	const gptCompletion = await openAI.createCompletion('text-ada-001', {
-		prompt: `${tickersList} I'm thinking of buying the following stock tickers: `,
+		prompt: `${filteredTickers} I'm thinking of buying the following stock tickers: `,
 		temperature: 0.7,
 		max_tokens: 32,
 		top_p: 1,
@@ -70,30 +70,40 @@ export const helloWorld = functions.https.onRequest(async (request, response) =>
 	const choices = gptCompletion.data.choices
 	
 	if (choices != undefined && choices != null && choices[0] != undefined && choices[0] != null) {
-		const stocksToBuy = choices[0].text?.match(/\b[A-Z]+\b/g)
+		const stocksToBuy = choices[0].text?.match(/\b[A-Z]+\b/g)?.map((s) => {return s})
 
 		console.log("got stocks to buy: " + stocksToBuy)
 
-		const account = await alpaca.getAccount().catch((error: unknown) => response.send(error))
-
-		console.log("got alpaca account")
-	
-		if (stocksToBuy != undefined && stocksToBuy != null) {
-			const order = await alpaca.createOrder({
-				symbol: stocksToBuy[Math.floor(Math.random() * stocksToBuy?.length)],
-				notional: account.buying_power * 0.1,
-				side: 'buy',
-				type: 'market',
-				time_in_force: 'day'
-			})
-
-			console.log("created order")
-		
-			response.send(order);
-		} else {
-			response.send("no stocks to buy")
-		}
-	} else {
-		response.send("choices empty")
+		return stocksToBuy
 	}
-});
+
+	return
+}
+
+/**
+ * Makes a single order using a random ticker in the stocksToBuy list (also sends response)
+ */
+async function makeOrder(stocksToBuy: string[] | undefined, tickers: Map<string, Ticker>, response: functions.Response<any>) {
+	
+	const account = await alpaca.getAccount().catch((error: unknown) => response.send(error))
+
+	console.log("got alpaca account")
+
+	if (stocksToBuy != undefined && stocksToBuy != null) {
+		const stockToBuy = stocksToBuy[Math.floor(Math.random() * stocksToBuy?.length)]
+		const order = await alpaca.createOrder({
+			symbol: stockToBuy,
+			notional: 0.1 * account.buying_power * tickers.size > 0 ? tickers.get(stockToBuy)!.getRating() : 1,
+			side: 'buy',
+			type: 'market',
+			time_in_force: 'day'
+		})
+
+		console.log("created order")
+
+		response.send(order)
+	} else {
+		response.send("no stocks to buy")
+	}
+}
+
--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -1,3 +1,5 @@
+import { Ticker } from "./classes"
+
 /* eslint-disable require-jsdoc */
 export function marshallHistoryFile() {// save stored user scores after running calculation
 	console.log("not implemented")
@@ -5,4 +7,24 @@ export function marshallHistoryFile() {// save stored user scores after running 
 
 export function unmarshallHistoryFile() {// output users scores to a map/object/dict
 	console.log("not implemented")
+}
+
+export function addTickersToMap(retreivedTickers: Ticker[], tickers: Map<string, Ticker>, sourceMultiplier: number) : void {
+	retreivedTickers.forEach((ticker) => {
+		const name = ticker.getName()
+		tickers.set(name, tickers.get(name) != undefined ?
+			tickers.get(name)!.multiplyRating(sourceMultiplier * ticker.getRating())
+			: new Ticker(name).multiplyRating(sourceMultiplier * ticker.getRating())
+		)
+	})
+}
+
+export function filterToAboveAverage(tickers: Map<string, Ticker>) : string[] {
+	let averageScore = 0
+	Object.values(tickers).forEach((ticker) => averageScore += ticker.getRating())
+	averageScore = averageScore / Object.values(tickers).length
+
+	const filteredTickers = Object.keys(tickers).filter((ticker) => tickers.get(ticker)!.getRating() > averageScore)
+	console.log("filtered below average tickers out of map")
+	return filteredTickers
 }
