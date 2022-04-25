564985f52e74e1848c54906adf48ce21d476fa89
nathanstephenson <nathans261@gmail.com>
2022-04-25 21:48:57 +0100

    addded "unusual_whales" to twitter user list

--- functions/src/sources/twitter/users.ts
+++ functions/src/sources/twitter/users.ts
@@ -6,6 +6,7 @@ export const users = (): Map<string, TwitterUser> => {
 	users.set("Elon Musk", 			new TwitterUser("Jim Cramer", 					"jimcramer", 					-5))
 	users.set("Inverse Jim Cramer", new TwitterUser("Inverse Jim Cramer", 			"CramerTracker", 				10))
 	users.set("Elon Musk", 			new TwitterUser("Elon Musk", 					"elonmusk", 					-5))
+	users.set("Unusual Whales", 	new TwitterUser("Unusual Whales", 				"unusual_whales", 				10))
 
 	return users
 }
