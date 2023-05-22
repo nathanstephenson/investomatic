dda6f7ff5c9bee6145c401773e82216163ffa45a
nathanstephenson <nathans261@gmail.com>
2023-05-22 23:40:02 +0100

    enabled body parsing and separated requests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, {Express, Request, Response} from 'express'
+import bodyParser from 'body-parser'
 import {exec} from 'child_process'
 import Alpaca from "@alpacahq/alpaca-trade-api"
 
@@ -12,6 +13,8 @@ import { getHistoricScores } from "./sources/market"
 
 const port = process.env.PORT || 8080
 const app: Express = express()
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 const alpaca = new Alpaca({
 	keyId: alpacaKeyID,
 	secretKey: alpacaAPIKey,
@@ -41,68 +44,75 @@ function execAlgo(): Promise<string> {
 	})	
 }
 
+interface Output {
+	score: number,
+	data: OutputData[]
+}
+
 interface OutputData {
 	value: number,
 	timestamp: number
 }
-let outputData = new Map<string, OutputData[]>()
+let outputData = new Map<string, Output>()
 
-app.get('/*', async function(req: Request, res: Response) {
+app.get('/data', async function(req: Request, res: Response) {
+	setResponseHeaders(res)
+	const tickersForHistory = getReqData(req.url).split(DATA_SEPARATOR).map(tickerData => {
+		const splitData = tickerData.split(SUB_DATA_SEPARATOR)
+		return {name: splitData[0], startDate: Number.parseInt(splitData[1]) || 1}
+	})
+	res.send(await getHistoricScores(tickersForHistory))
+})
+
+app.get('/visualise', function(req: Request, res: Response) {
+	setResponseHeaders(res)
+	const reqData = getReqData(req.url)
+	res.setHeader("ticker", reqData)
+	res.send(outputData.get(reqData))
+})
+
+app.get('/tickers', function(req: Request, res: Response) {
+	setResponseHeaders(res)
+	res.send(Array.from(outputData.keys()))
+})
+
+app.get('/exec', async function(req: Request, res: Response) {
+	setResponseHeaders(res)
+	await execAlgo().then(() => res.send(true), () => res.send(false))
+})
+
+app.get('/order', function(req: Request, res: Response) {
+	setResponseHeaders(res)
+	const orderTickers: Ticker[] = getReqData(req.url).split(SUB_DATA_SEPARATOR).map(tickerData => {
+		const splitData: string[] = tickerData.split(DATA_KEYVAL_SEPARATOR)
+		return new Ticker(splitData[0], Number.parseFloat(splitData[1]) || 1)
+	})
+	res.send(makeOrder(orderTickers, res))
+})
+
+app.post('/output', async function(req: Request, res: Response) {
+	setResponseHeaders(res)
+	outputData = new Map<string, Output>()
+	for(const ticker in req.body) {
+		outputData.set(ticker, req.body[ticker])
+	}
+	res.send(true)
+})
+
+function setResponseHeaders(res: Response) {
 	res.setHeader("Access-Control-Allow-Origin", "*")
 	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE")
 	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-	if(req.url === '/favicon.ico'){ return }
-	let indexOfReqData: number = req.url.indexOf(DATA_SEPARATOR) 
+}
+
+function getReqData(reqUrl: string): string {
+	let indexOfReqData: number = reqUrl.indexOf(DATA_SEPARATOR) 
 	if(indexOfReqData == -1) {
-		indexOfReqData = req.url.length 
+		indexOfReqData = reqUrl.length 
 	}
-	const reqType = req.url.substring(1, indexOfReqData)
-	const reqData = req.url.substring(indexOfReqData + 1)
-	// console.log(`req type: ${reqType}, req data: ${reqData}`)
-	switch(reqType) {
-		case 'data': // data?{ticker}&{start date}?{ticker}&{start date}
-			const tickersForHistory = reqData.split(DATA_SEPARATOR).map(tickerData => {
-				const splitData = tickerData.split(SUB_DATA_SEPARATOR)
-				return {name: splitData[0], startDate: Number.parseInt(splitData[1]) || 1}
-			})
-			res.send(await getHistoricScores(tickersForHistory))
-			break
-		case 'output': // output?{ticker}&{value}={date}&{value}={date}?{ticker}&{value}={date}
-			outputData = new Map<string, OutputData[]>()
-			reqData.split(DATA_SEPARATOR).forEach(data => {
-				const ticker = data.split(SUB_DATA_SEPARATOR)[0]
-				const values = data.split(SUB_DATA_SEPARATOR).slice(1).map(keyVal => {
-					const splitKeyVal = keyVal.split(DATA_KEYVAL_SEPARATOR)
-					return {value: Number.parseFloat(splitKeyVal[0]), timestamp: Number.parseInt(splitKeyVal[1])}
-				})
-				outputData.set(ticker, values)
-				console.log("cached output data for ", ticker)
-			})
-			res.send(true)
-			break
-		case 'visualise': // visualise?{ticker}
-			res.setHeader("ticker", reqData)
-			res.status(200)
-			res.send(outputData.get(reqData))
-			break
-		case 'tickers': // tickers
-			res.send(Array.from(outputData.keys()))
-			break
-		case 'exec': // exec
-			await execAlgo().then(() => res.send(true), () => res.send(false))
-			break
-		case 'order': // order?{ticker}&{score}?{ticker}&{score}
-			const orderTickers: Ticker[] = reqData.split(SUB_DATA_SEPARATOR).map(tickerData => {
-				const splitData: string[] = tickerData.split(DATA_KEYVAL_SEPARATOR)
-				return new Ticker(splitData[0], Number.parseFloat(splitData[1]) || 1)
-			})
-			res.send(makeOrder(orderTickers, res))
-			break
-		default:
-			res.send("You probably shouldn't be here")
-			break
-	}
-})
+
+	return reqUrl.substring(indexOfReqData + 1)
+}
 
 /**
  * Makes a single order using a random ticker in the stocksToBuy list (also sends response)
